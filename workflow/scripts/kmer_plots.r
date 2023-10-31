7f39597a8fa2adb1d766112e075edbdc464facae
Benjamin J Perry <ben.perry@agresearch.co.nz>
2023-10-31 13:50:39 +1300

    more tweaks to plotting

--- workflow/scripts/kmer_plots.r
+++ workflow/scripts/kmer_plots.r
@@ -263,7 +263,7 @@ draw_entropy_heatmap <- function(datamatrix, output_folder, heatmap_image_file, 
        trace = "none", breaks = min(datamatrix) + (max(datamatrix)-min(datamatrix))/9*seq(0,9), 
        col = cm , key=FALSE, density.info="none", 
        keysize=1.0, margin=c(80,60), cexRow=1.3, cexCol=1.3, 
-       lmat=rbind(  c(4,3,0 ), c(2, 1, 0) ), lwid=c(.2, .8, 0 ), lhei=c(.5, 3) , labRow = FALSE, labCol=colLabels, ylab = "kmer")
+       lmat=rbind(  c(4,3,0 ), c(2, 1, 0) ), lwid=c(.2, .8, 0 ), lhei=c(.5, 3) , labRow = rownames(datamatrix), labCol=colLabels)
    dev.off()
 
    write.table(colnames(datamatrix)[hm$colInd[1:length(hm$colInd)]] , file="samplenames_ordered_as_heatmap.txt",row.names=TRUE,sep="\t") 
